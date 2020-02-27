be9efbe171f3b9d073ae1d97c8fee2252cf9a7d5
Volodymyr Tytarenko <bovatitar@gmail.com>
2020-02-27 14:50:35 +0100

    * Change to trait

--- src/Model/TitarLabModel.php
+++ src/Model/TitarLabModel.php
@@ -4,7 +4,16 @@
 namespace TitarLab\Model;
 
 
-class TitarLabModel
+trait TitarLabModel
 {
+    abstract protected function show($item);
+
+    protected function showList($array){
+        $list = [];
+        foreach ($array as $item){
+            $list[] = $this->show($item);
+        }
+        return $list;
+    }
 
 }
